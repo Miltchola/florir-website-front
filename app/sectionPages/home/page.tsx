1079076c7a25ec2939902a09b7330c618c07e530
Miltchola <miltonkiefermello@gmail.com>
2025-08-22 13:02:24 -0300

    Feat: Style Pattern Creation

--- app/sectionPages/home/page.tsx
+++ app/sectionPages/home/page.tsx
@@ -2,18 +2,36 @@
 import { Button } from "@/app/sharedComponents/ui/Button";
 import { Line } from "@/app/sharedComponents/ui/Line";
 import { SectionDivision } from "@/app/sharedComponents/ui/SectionDivision";
+import { SectionTitle } from "@/app/sharedComponents/ui/SectionTitle";
 
+import { HeroSection } from "./components/HeroSection";
 
 export default function Home() {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-background-primary">
-        <h1 className="text-4xl font-regular text-font-primary mb-4">Welcome to Florir</h1>
-        <p className="text-lg text-font-secondary mb-8">Your journey to a greener life starts here.</p>
-        <Button text="Ver Mais" onClick={() => alert('Button Clicked!')} />
-        <Line/>
-        <div className="w-full flex justify-start px-16">
-            <SectionDivision text="Section Title"/>
-        </div>
+            <HeroSection
+                logo="/icons/Mini Logo Florir.png"
+                title="Flores lindas para qualquer ocasião"
+                text="Dê vida a sua casa com arranjos florais desidratados personalizados"
+                image="/images/Flor sem fundo.png"
+                buttonText="Ver Mais"
+            />
+            
+            
+            <SectionTitle
+                    image="/icons/Mini Logo Florir.png"
+                    title="Catálogo Florir"
+                    text="Todos os arranjos são criados com flores
+                        naturais desidratadas manualmente,
+                        respeitando a sazonalidade e exclusividade.
+                        Cada peça é única e feita com muito carinho
+                        especialmente para você."
+                />
+            <Button text="Ver Mais" onClick={() => alert('Button Clicked!')} />
+            <Line/>
+            <div className="w-full flex justify-start px-16">
+                <SectionDivision text="Section Title"/>
+            </div>
         </div>
     );
 }
